c7c7fb9e77ac9a25759abea5f0fd6dc82fea5443
soobinyoon <binyvvv@gmail.com>
2026-02-19 18:38:34 +0900

    feat: updated configs

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,21 @@
 return {
-  -- Gruvbox Dark
   {
-    "ellisonleao/gruvbox.nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      transparent_mode = true,
-      overrides = {
-        NormalFloat = { bg = "none" },
-        FloatBorder = { bg = "none" },
-        FloatTitle = { bg = "none" },
+      style = "night",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
       },
     },
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "tokyonight",
     },
   },
 }
